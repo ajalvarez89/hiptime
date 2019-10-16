dfd0f11d90d8e37538396a41bcf3b7d487823b68
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-10-16 10:35:11 -0500

    ep-73 ability to complete tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_210701) do
+ActiveRecord::Schema.define(version: 2019_10_16_145908) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_210701) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
